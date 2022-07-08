26340bc98601c943c2c3952b877d420fcf5a3c92
awoomouse <107973554+awoomouse@users.noreply.github.com>
2022-07-08 21:56:41 +0500

    Add files via upload

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,13 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -10,26 +16,103 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-
+        String sqlCommand = "CREATE TABLE user_table (Id INT PRIMARY KEY AUTO_INCREMENT, user_name VARCHAR(20), user_lastname VARCHAR(30), user_age INT)";
+        Connection con = Util.getCon();
+        try {
+            Statement st = con.createStatement();
+            st.executeUpdate(sqlCommand);
+        } catch (SQLException e) {
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+            }
+        }
     }
 
     public void dropUsersTable() {
-
+        String sqlCommand = "DROP TABLE user_table";
+        Connection con = Util.getCon();
+        try {
+            Statement st = con.createStatement();
+            st.executeUpdate(sqlCommand);
+        } catch (SQLException e) {
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+            }
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String sqlCommand = "INSERT user_table(user_name, user_lastname, user_age) VALUES (\'" + name + "\', \'" + lastName + "\', " + age + ")";
+        Connection con = Util.getCon();
+        try {
+            Statement st = con.createStatement();
+            st.executeUpdate(sqlCommand);
+        } catch (SQLException e) {
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+            }
+        }
     }
 
     public void removeUserById(long id) {
-
+        String sqlCommand = "DELETE FROM user_table WHERE Id=" + id;
+        Connection con = Util.getCon();
+        try {
+            Statement st = con.createStatement();
+            st.executeUpdate(sqlCommand);
+        } catch (SQLException e) {
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+            }
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        String sqlCommand = "SELECT Id, user_name, user_lastname, user_age FROM user_table";
+        List<User> list = new ArrayList<>();
+        Connection con = Util.getCon();
+        try {
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(sqlCommand);
+            while (rs.next()) {
+                User user = new User();
+                user.setId((long) rs.getInt(1));
+                user.setName(rs.getString(2));
+                user.setLastName(rs.getString(3));
+                user.setAge((byte) rs.getInt(4));
+                list.add(user);
+            }
+        } catch (SQLException e) {
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+            }
+        }
+        return list;
     }
 
     public void cleanUsersTable() {
 
+        String sqlCommand = "TRUNCATE TABLE user_table";
+        Connection con = Util.getCon();
+        try {
+            Statement st = con.createStatement();
+            st.execute(sqlCommand);
+        } catch (SQLException e) {
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+            }
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,38 @@
 package jm.task.core.jdbc.util;
 
+import java.lang.reflect.InvocationTargetException;
+import java.sql.*;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String url = "jdbc:mysql://localhost:3306/test";
+    private static final String user = "root";
+    private static final String password = "root";
+
+    private static Connection connection;
+
+//    public static void main(String[] args) {
+//        try {
+//            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+//            connection = DriverManager.getConnection(url, user, password);
+//            Statement st = connection.createStatement();
+//            String sqlCommandCreate = "CREATE TABLE user_table (Id INT PRIMARY KEY AUTO_INCREMENT, user_name VARCHAR(20), user_lastname VARCHAR(30), user_age INT)";
+//            String sqlCommand = "DROP TABLE user_table";
+//            st.executeUpdate(sqlCommand);
+//            connection.close();
+//            System.out.println("done!");
+//        } catch (SQLException e) {
+//            System.out.println("sql except");
+//        } catch (ClassNotFoundException e) {
+//            System.out.println("class not found");
+//        } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException | InstantiationException e) {
+//            System.out.println("chto-to sluchilos' s Class.forname()");
+//        }
+//    }
+    public static Connection getCon() {
+        try {
+            return connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException ex) {
+        }
+        return connection;
+    }
 }
