a2086ea5358efd2a3a359b668f05fce7d32d3b10
awoomouse <107973554+awoomouse@users.noreply.github.com>
2022-07-09 19:18:56 +0500

    Update Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -10,24 +10,6 @@ public class Util {
 
     private static Connection connection;
 
-//    public static void main(String[] args) {
-//        try {
-//            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
-//            connection = DriverManager.getConnection(url, user, password);
-//            Statement st = connection.createStatement();
-//            String sqlCommandCreate = "CREATE TABLE user_table (Id INT PRIMARY KEY AUTO_INCREMENT, user_name VARCHAR(20), user_lastname VARCHAR(30), user_age INT)";
-//            String sqlCommand = "DROP TABLE user_table";
-//            st.executeUpdate(sqlCommand);
-//            connection.close();
-//            System.out.println("done!");
-//        } catch (SQLException e) {
-//            System.out.println("sql except");
-//        } catch (ClassNotFoundException e) {
-//            System.out.println("class not found");
-//        } catch (InvocationTargetException | IllegalAccessException | NoSuchMethodException | InstantiationException e) {
-//            System.out.println("chto-to sluchilos' s Class.forname()");
-//        }
-//    }
     public static Connection getCon() {
         try {
             return connection = DriverManager.getConnection(url, user, password);
